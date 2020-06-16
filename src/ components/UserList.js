e10786a8b1d29ce97f5452232a0457ea5d4e9bd2
YamanKatmawi <yamankatmawi@gmx.de>
2020-06-16 10:13:48 +0200

    first commit

---
+++ src/ components/UserList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const UserList = (props) => {
+  const toDos = props.list.map((item, i) => (
+    <li key={i}>
+      {item}{" "}
+      <i
+        onClick={() => {
+          console.log(item);
+          props.del(item);
+        }}
+      >
+        🗑
+      </i>
+    </li>
+  ));
+  return <ul>{toDos}</ul>;
+};
+
+export default UserList;
